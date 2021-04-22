c622d7a8bba2a0cc96744d529f1ba8ab388bd018
Gurjit <Gurjit@localhost>
2021-04-22 14:32:17 -0400

    tuition and financing page

---
+++ TFTDynamic/src/main/java/pages/tuitionAndFinancingPage.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import base.TestBase;
+
+public class tuitionAndFinancingPage extends TestBase{
+
+	@FindBy(linkText= "Pay Upfront")
+	WebElement payUpfrontLink;
+	
+	@FindBy(linkText= "Contact Us")
+	WebElement contactUsLink;
+	
+	public tuitionAndFinancingPage() {
+	
+	PageFactory.initElements(driver, this);	
+	
+	}
+	
+	public void tuitionAndFinancingLink() {
+		tuitionAndFinancingLink.click();
+		
+	}
+	
+	public void contactUsLink() {
+		contactUsLink.click();
+	}
+	
+	public String getHomeTitle() {
+		return driver.getTitle();
+	}
+	
+		
+		
+}
